358904a8bd081dc61e91363e9831492e0242badb
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-07-14 01:49:04 +0900

    게시글 로딩효과 추가

--- src/Components/board/Post.jsx
+++ src/Components/board/Post.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import Load from '../Load';
 import queryString from 'query-string';
 import Topmenu from '../Topmenu';
 import axios from 'axios';
@@ -7,6 +8,7 @@ import axios from 'axios';
 const Content = ()=> {    
     const { search } = useLocation();
     const { no } = queryString.parse(search);
+    const [nowload, setNowload] = useState(true);
     const [title, setTitle] = useState();
     const body = useRef(null);
     const [date, setDate] = useState();
@@ -18,6 +20,7 @@ const Content = ()=> {
                 no : no
             }
         }).then((res)=>{
+            setNowload(false);
             setTitle(res.data.title);
             setDate(res.data.createdAt.substr(0, 10));
             body.current.innerText = res.data.body; 
@@ -27,19 +30,23 @@ const Content = ()=> {
 
     },[])
     
-
-    return(
-        <>
-            <div className="post-title-wrap">
-                <h2 className="post-title">{title}</h2>
-                <p className="post-date">{date}</p>
-            </div>
+    if(nowload){//로딩중
+        return (<Load/>);
+    }else{
+        return(
+            <>
+                <div className="post-title-wrap">
+                    <h2 className="post-title">{title}</h2>
+                    <p className="post-date">{date}</p>
+                </div>
+                
+                <div ref={body} className="post-content">
+                </div>
             
-            <div ref={body} className="post-content">
-            </div>
-        
-        </>
-    )
+            </>
+        )
+    }
+    
 }
 
 export default Content;
